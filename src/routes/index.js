c712959ee7ec00f24a2562a98db55f0c8bc62a8c
nova zaky fathoni <novazaky1@gmail.com>
2024-10-25 13:08:26 +0700

    Feature: Recommendation Page

--- src/routes/index.js
+++ src/routes/index.js
@@ -25,7 +25,7 @@ router.get('/', IndexController.loginPage);
 router.get('/register', IndexController.registerPage);
 router.get('/dashboard', auth, IndexController.dashboard)
 router.get('/history', auth, IndexController.history)
-router.get('/recommendation', auth, IndexController.recommendation)
+router.get('/recommendation', IndexController.recommendation)
 router.get('/users', auth, IndexController.users)
 router.get('/schools', auth, IndexController.schools)
 router.get('/devices', auth, IndexController.devices)
