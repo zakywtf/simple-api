ba268ac78fd0171e4733b0e8f9e66aaa7e36d4fa
nova zaky fathoni <novazaky1@gmail.com>
2025-03-09 15:46:02 +0700

    Feature: Init Stock

--- src/models/MaterialsModel.js
+++ src/models/MaterialsModel.js
@@ -1,5 +1,6 @@
 import Models from "../classes/classModel";
 import sch from "../schemas/materials";
+import stocks from "../schemas/stocks";
 
 class materialsModel extends Models{
     constructor(){
@@ -9,9 +10,13 @@ class materialsModel extends Models{
     async insert(obj){
         obj.store_id = this.udata.store_id
         let resp = await this.model.create(this.convertParam(obj, false))
-
+        await this.saveInitStock(this.udata.store_id, resp._id)
         return this.insert_result(resp)
 
+    }
+
+    async saveInitStock(store_id, material_id){
+        await stocks.create({store_id: store_id, material_id: material_id, amount: 0})
     }
 }
 
